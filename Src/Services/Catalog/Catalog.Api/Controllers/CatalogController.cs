9adbacaace678facae1c2098332f3d4eb8e72ff4
Arash Mehdipour <arash201221@gmail.com>
2022-08-08 22:12:37 +0430

    Create Presentation Layer Create GetProducts ActionRersult

---
+++ Src/Services/Catalog/Catalog.Api/Controllers/CatalogController.cs
@@ -0,0 +1,29 @@
+﻿using Catalog.Api.Entities;
+using Catalog.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Catalog.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ILogger _logger;
+
+        public CatalogController(IProductRepository productRepository, ILogger logger)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetProducts();
+            return Ok(products);
+        }
+    }
+}
