a1ab2a2090bc9571258891dd118dad2676798bc1
Arash Mehdipour <arash201221@gmail.com>
2022-08-12 00:25:29 +0430

    Create Basket.Api Project

---
+++ Src/Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+        public ShoppingCart()
+        {
+
+        }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+        public string UserName { get; set; }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                {
+                    totalPrice = item.Price * item.Quntity;
+                }
+                return totalPrice;
+            }
+        }
+    }
+}
